3897a2b41ffacb0167e50b0928991e0c79ceecaf
anuradha-bhandari <131469707+anuradha-bhandari@users.noreply.github.com>
2025-08-15 20:56:48 +0530

    project submission

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import MovieDetail from './component/movieDetailPage';
+import MovieDetail from './component/moviedetailpage';
 import Upcoming from './component/upcoming';
 import TopRating from './component/toprated';
 import Navbar from './component/navbar';
 
 import Popular from './component/popularmovies';
-import SearchMovie from './component/searchPage';
+import SearchMovie from './component/searchpage';;
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
 
